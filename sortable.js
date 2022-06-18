dc9f2498dcb88e2e75483ca3d414cb67268df329
nsym-coding <68327738+nsym-coding@users.noreply.github.com>
2022-06-18 23:09:19 +0100

    just added some comments and tried to show an alert if the user searches for a non existent hero

--- sortable.js
+++ sortable.js
@@ -1,3 +1,9 @@
+/**
+ * This async function will use fetch to make a request to the 
+ * api and if the promise is successfull resolved it will then convert the 
+ * response into a json object which is then returned.
+ * @returns if successful it will return an object that is an array of json data
+ */
 async function getHeroes() {
     let url = 'https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json';
     try {
@@ -6,22 +12,31 @@ async function getHeroes() {
     } catch (error) {
         console.log(error);
     }
-    console.log(res);
+    console.log("res check",typeof res);
 }
 
+/**
+ * This async function waits for getHeroes to finish fetching the data
+ * before then executing the rest of the code inside this function.
+ * The search bar is initialised in here and depending on whether or not 
+ * the user enters characters into the search bar it will call the create
+ * table function to either create a table based on the original data or 
+ * based on the search results.
+ */
 async function renderHeroes() {
     let heroes = await getHeroes();
-    console.log(typeof heroes)
+    
 
     var searchBar = document.createElement("input");
     searchBar.setAttribute('type', 'text')
+    searchBar.setAttribute('placeholder', 'type your search in here!')
     document.body.appendChild(searchBar);
 
     searchBar.addEventListener('keyup', event => {
         var characters = event.target.value.toLowerCase()
 
         var fHeroes = heroes.filter(hero => hero.name.toLowerCase().includes(characters))
-        console.log(fHeroes)
+        
 
         if (fHeroes.length !== 0) {
             var oldTable = document.querySelector('tbody')
@@ -29,6 +44,14 @@ async function renderHeroes() {
             var oldHeader = document.querySelector('thead')
             oldHeader.remove()
             createTable(fHeroes)
+        }else if (fHeroes.length === 0){
+            var oldTable = document.querySelector('tbody')
+            oldTable.remove()
+            var oldHeader = document.querySelector('thead')
+            oldHeader.remove()
+            window.alert("THAT HERO DOESN'T EXIST, TRY AGAIN")
+            
+            createTable(heroes)
         }
     })
 
@@ -37,7 +60,10 @@ async function renderHeroes() {
 
 renderHeroes();
 
-
+/**
+ * Function to create a table based off of the array that's passed in.
+ * @param {This will be either the original heroes array or the fHeroes array after a search has been made} list 
+ */
 function createTable(list) {
 
     var table = document.createElement("table");  //makes a table element for the page
@@ -62,6 +88,7 @@ function createTable(list) {
         row.insertCell(14).innerHTML = list[i].biography.alignment;
     }
 
+   
     var headers = ["Icon", "Name", "Full Name", "Intelligence", "Strength", "Speed", "Durability", "Power", "Combat", "Race", "Gender", "Height", "Weight", "Place of Birth", "Alignment"];
 
     var header = table.createTHead();
@@ -70,4 +97,5 @@ function createTable(list) {
         headerRow.insertCell(i).innerHTML = headers[i];
     }
     document.body.append(table)
+    
 }
