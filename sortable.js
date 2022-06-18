bffeb68b0a15acab59ce67fe075742393e0ad982
Your Name <n.d.russell@hotmail.com>
2022-06-18 20:57:26 +0100

    search bar

--- sortable.js
+++ sortable.js
@@ -11,38 +11,63 @@ async function getHeroes() {
 
 async function renderHeroes() {
     let heroes = await getHeroes();
+    console.log(typeof heroes)
 
-    var headers = ["Icon", "Name", "Full Name", "Intelligence", "Strength", "Speed", "Durability", "Power", "Combat", "Race", "Gender", "Height", "Weight", "Place of Birth", "Alignment"];
-    var table = document.createElement("table");  //makes a table element for the page
-        
-    for(var i = 0; i < heroes.length; i++) {
-        var row = table.insertRow(i);
-        row.insertCell(0).innerHTML = '<img src =' +heroes[i].images.xs+'>';
-        row.insertCell(1).innerHTML = heroes[i].name;        
-        row.insertCell(2).innerHTML = heroes[i].biography.fullName;
-        row.insertCell(3).innerHTML = heroes[i].powerstats.intelligence;
-        row.insertCell(4).innerHTML = heroes[i].powerstats.strength;
-        row.insertCell(5).innerHTML = heroes[i].powerstats.speed;
-        row.insertCell(6).innerHTML = heroes[i].powerstats.durability;
-        row.insertCell(7).innerHTML = heroes[i].powerstats.power;
-        row.insertCell(8).innerHTML = heroes[i].powerstats.combat;        
-        row.insertCell(9).innerHTML = heroes[i].appearance.race;
-        row.insertCell(10).innerHTML = heroes[i].appearance.gender;
-        row.insertCell(11).innerHTML = heroes[i].appearance.height[1];
-        row.insertCell(12).innerHTML = heroes[i].appearance.weight[1];
-        row.insertCell(13).innerHTML = heroes[i].biography.placeofBirth;
-        row.insertCell(14).innerHTML = heroes[i].biography.alignment;
-    }
+    var searchBar = document.createElement("input");
+    searchBar.setAttribute('type', 'text')
+    document.body.appendChild(searchBar);
 
-    var header = table.createTHead();
-    var headerRow = header.insertRow(0);
-    for(var i = 0; i < headers.length; i++) {
-        headerRow.insertCell(i).innerHTML = headers[i];
-    }
+    searchBar.addEventListener('keyup', event => {
+        var characters = event.target.value.toLowerCase()
 
-    console.log(table.height);
+        var fHeroes = heroes.filter(hero => hero.name.toLowerCase().includes(characters))
+        console.log(fHeroes)
 
-    document.body.append(table);
+        if (fHeroes.length !== 0) {
+            var oldTable = document.querySelector('tbody')
+            oldTable.remove()
+            var oldHeader = document.querySelector('thead')
+            oldHeader.remove()
+            createTable(fHeroes)
+        }
+    })
+
+    createTable(heroes)
 }
 
 renderHeroes();
+
+
+function createTable(list) {
+
+    var table = document.createElement("table");  //makes a table element for the page
+
+
+    for (var i = 0; i < list.length; i++) {
+        var row = table.insertRow(i);
+        row.insertCell(0).innerHTML = '<img src =' + list[i].images.xs + '>';
+        row.insertCell(1).innerHTML = list[i].name;
+        row.insertCell(2).innerHTML = list[i].biography.fullName;
+        row.insertCell(3).innerHTML = list[i].powerstats.intelligence;
+        row.insertCell(4).innerHTML = list[i].powerstats.strength;
+        row.insertCell(5).innerHTML = list[i].powerstats.speed;
+        row.insertCell(6).innerHTML = list[i].powerstats.durability;
+        row.insertCell(7).innerHTML = list[i].powerstats.power;
+        row.insertCell(8).innerHTML = list[i].powerstats.combat;
+        row.insertCell(9).innerHTML = list[i].appearance.race;
+        row.insertCell(10).innerHTML = list[i].appearance.gender;
+        row.insertCell(11).innerHTML = list[i].appearance.height[1];
+        row.insertCell(12).innerHTML = list[i].appearance.weight[1];
+        row.insertCell(13).innerHTML = list[i].biography.placeofBirth;
+        row.insertCell(14).innerHTML = list[i].biography.alignment;
+    }
+    
+    var headers = ["Icon", "Name", "Full Name", "Intelligence", "Strength", "Speed", "Durability", "Power", "Combat", "Race", "Gender", "Height", "Weight", "Place of Birth", "Alignment"];
+
+    var header = table.createTHead();
+    var headerRow = header.insertRow(0);
+    for (var i = 0; i < headers.length; i++) {
+        headerRow.insertCell(i).innerHTML = headers[i];
+    }
+    document.body.append(table)
+}
